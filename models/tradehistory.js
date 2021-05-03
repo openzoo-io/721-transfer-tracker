b7d49b1eb115e94d8e77e6f4a847f869ed91358d
jason <kwon_jason@yahoo.com>
2021-05-03 18:36:16 +0300

    update

--- models/tradehistory.js
+++ models/tradehistory.js
@@ -2,12 +2,14 @@ const mongoose = require('mongoose')
 
 const TradeHistory = mongoose.Schema(
   {
-    erc721address: { type: String, required: true },
+    collectionAddress: { type: String, required: true },
     from: { type: String, required: true },
     to: { type: String, required: true },
     tokenID: { type: Number, required: true },
     price: { type: Number, required: true },
+    value: { type: Number, default: 1 },
     saleDate: { type: Date, default: new Date() },
+    isAuction: { type: Boolean, default: false },
   },
   {
     timestamps: true,
