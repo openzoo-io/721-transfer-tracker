0cf90baedbdaffab2c2196c34764ae49ccb3f3e0
jason <kwon_jason@yahoo.com>
2021-05-12 20:36:05 +0300

    update case insensitive

--- services/collectiontracker.js
+++ services/collectiontracker.js
@@ -106,62 +106,6 @@ const trackERC721Distribution = async (contracts) => {
   } catch (error) {}
 }
 
-const _trackERC721Distribution = async (contracts) => {
-  let scs = new Map()
-  let totalSupplies = new Map()
-  contracts.map((contract) => {
-    scs.set(
-      contract.address,
-      contractutils.loadContractFromAddress(contract.address),
-    )
-    totalSupplies.set(contract.address, 1)
-  })
-
-  let total = contracts.length
-  let tokenID = 1
-  while (total > 0) {
-    const promises = contracts.map(async (contract) => {
-      let sc = scs.get(contract.address)
-      let supply = totalSupplies.get(contract.address)
-      if (supply == 1) {
-        try {
-          let tokenURI = await sc.tokenURI(tokenID)
-          if (!tokenURI.startsWith('https://')) {
-          } else {
-            let to = await sc.ownerOf(tokenID)
-            let erc721token = await ERC721TOKEN.findOne({
-              contractAddress: contract.address,
-              tokenID: tokenID,
-            })
-            if (erc721token) {
-              if (erc721token.owner != to) {
-                erc721token.owner = to
-                await erc721token.save()
-              }
-            } else {
-              if (tokenURI.startsWith('https://')) {
-                let newTk = new ERC721TOKEN()
-                newTk.contractAddress = contract.address
-                newTk.tokenID = tokenID
-                newTk.tokenURI = tokenURI
-                newTk.owner = to
-                await newTk.save()
-              }
-            }
-          }
-        } catch (error) {
-          console.log('error')
-          console.log(totalSupplies.get(contract.address))
-          totalSupplies.set(contract.address, 0)
-          total--
-        }
-      }
-    })
-    await Promise.all(promises)
-    tokenID++
-  }
-}
-
 const collectionTracker = {
   trackCollectionTransfer,
   trackERC721Distribution,
