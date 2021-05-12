ede1cf282554ffbcafc20ed93ccd431d48b57d7a
jason <kwon_jason@yahoo.com>
2021-05-12 18:20:31 +0300

    update auction sc

--- models/auction.js
+++ models/auction.js
@@ -1,10 +1,13 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 const Auction = mongoose.Schema({
-  minter: { type: String, required: true, index: true },
-  tokenID: { type: Number, required: true, index: true },
+  minter: { type: String, required: true },
+  tokenID: { type: Number, required: true },
+  bidder: { type: Number, required: true },
   startTime: { type: Number, default: Date.now },
   endTime: { type: Date, default: Date.now },
-});
+})
 
-mongoose.model("Auction", Auction);
+Auction.index({ minter: 1, tokenID: -1 }, { unique: true })
+
+mongoose.model('Auction', Auction)
