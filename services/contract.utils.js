8c0977a304e4676b8599d61384209321eab7c41a
jason <kwon_jason@yahoo.com>
2021-09-01 02:50:18 +1000

    fix

--- services/contract.utils.js
+++ services/contract.utils.js
@@ -2,13 +2,23 @@ require('dotenv').config()
 const ethers = require('ethers')
 
 const SimplifiedERC721ABI = require('../constants/simplifiederc721abi')
+const MinimalSC20ABI = require('../constants/mini_contract_abi.js')
 const rpcapi = process.env.NETWORK_RPC
 const chaindID = parseInt(process.env.NETWORK_CHAINID)
+const provider = new ethers.providers.JsonRpcProvider(rpcapi, chaindID)
+// symbol store
+const symbolStore = new Map()
+// name store
+const nameStore = new Map()
+
+const toLowerCase = (val) => {
+  if (val) return val.toLowerCase()
+  else return val
+}
 
 const loadContractFromAddress = (address) => {
   try {
     let abi = SimplifiedERC721ABI
-    let provider = new ethers.providers.JsonRpcProvider(rpcapi, chaindID)
     let contract = new ethers.Contract(address, abi, provider)
     return contract
   } catch (error) {}
@@ -21,9 +31,30 @@ const getTokenInfo = async (address, tkID) => {
   return uri
 }
 
+const getSymbol = async (address) => {
+  address = toLowerCase(address)
+  let symbol = symbolStore.get(address)
+  if (symbol) return symbol
+  let tokenContract = new ethers.Contract(address, MinimalSC20ABI, provider)
+  symbol = await tokenContract.symbol()
+  symbolStore.set(address, symbol)
+  return symbol
+}
+
+const getName = async (address) => {
+  address = toLowerCase(address)
+  let name = nameStore.get(address)
+  if (name) return name
+  let tokenContract = new ethers.Contract(address, MinimalSC20ABI, provider)
+  name = await tokenContract.name()
+  nameStore.set(address, name)
+  return name
+}
 const contractutils = {
   loadContractFromAddress,
   getTokenInfo,
+  getSymbol,
+  getName,
 }
 
 module.exports = contractutils
